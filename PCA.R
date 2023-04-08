652cc4b069af797627657ce9c439884fda94635e
chantore <wnrnrxla@gmail.com>
2023-04-09 00:43:05 +0900

    Update : PCA

--- PCA.R
+++ PCA.R
@@ -2,26 +2,32 @@ library(httr)
 library(devtools)
 library(ggplot2)
 library(ggbiplot)
+library(car)
 
-df <- read.csv("/Users/jch/Desktop/백제 대옹 연구/Bakje_DO.csv")
+#Data processing
+df <- read.csv("/Users/jch/Desktop/DO_TEST.csv")
+df <- df[1:13]
+df <- na.omit(df)
+df <- df[!(df$Type == "기타" ), ]
+df <- df[!(df$Type == "제외" ), ]
 
-df <- df[!(df$Type == "ETC" ), ]
-df <- df[!(df$Type == "Excep" ), ]
+df_category <- df[, 5]
+df_name <- df[, 4]
 
-df_category <- df[, 2]
-df_name <- df[, 1]
+df <- df[, -(1:5)]
 
-df <- df[, -2]
-df <- df[, -1]
+#Check Multicollinearity
+cor(df)
+pairs(df, panel=panel.smooth)
 
+#PCA
 prcomp_df <- prcomp(df, center=T, scale=T)
 
 summary(prcomp_df)
 
-pca_DO <- ggbiplot(prcomp_df, choices = c(1, 2), obs.scale = 1,
+pca_DO <- ggbiplot(prcomp_df, choices = c(1, 2), obs.scale = 1, labels=df_name,
                    var.scale = 1, groups = df_category) +
-  scale_color_discrete(name = '') +
-  theme(legend.direction = 'horizontal', legend.position = 'top')
+                   scale_color_discrete(name = '') +
+                   theme(legend.direction = 'horizontal', legend.position = 'top')
 
 print(pca_DO)
-
