42beca5c81a59694bae40c28ba9ce95d5dab920e
JCH <wnrnrxla@gmail.com>
2023-07-23 22:00:26 +0900

    Update : 20230719 수정사항

--- Script/DOKilnnum.R
+++ Script/DOKilnnum.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-df <- read.csv("/Users/jch/Desktop/DOKilnnum.csv")
+df <- read.csv("C:/github/P3K-HB_Pottery-Kiln/Data/DOKilnnum.csv")
 df <- na.omit(df)
 xx <- c(0, 1, 2, 3, 4, 5, 6)
 yy <- c(16, 3, 5, 0, 1, 0, 3)
@@ -9,9 +9,8 @@ yy <- c(16, 3, 5, 0, 1, 0, 3)
 
 ggplot() +
   geom_bar(data=df, aes(x=대옹출토가마)) +
-  geom_line(aes(x=xx, y=yy), linetype = "dashed", size = 0.5) +
   theme_bw() +
   labs(x="대옹출토 가마 수", y="유적 수") +
-  scale_y_continuous(limits = c(0, 16), breaks=seq(0, 16, 1), labels=scales::number) +
+  scale_y_continuous(limits = c(0, 15), breaks=seq(0, 15, 1), labels=scales::number) +
   scale_x_continuous(breaks=c(0, 1, 2, 3, 4, 5, 6), labels=c("0", "1", "2", "3", "4", "5", "6")) +
-ggsave("/Users/jch/Desktop/DOKilnnum.png", dpi=150, width=3.5, height=3.5, units='in')
+ggsave("C:/Users/JCH/OneDrive/Desktop/DOKilnnum.png", dpi=150, width=3.5, height=3.5, units='in')
