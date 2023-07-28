a301f6b87d42719e2fc68e616a937f6991e1df5d
JCH <wnrnrxla@gmail.com>
2023-07-28 16:50:48 +0900

    Update : 20230728 수정
    - 지역별 대옹 소비 양상 검토를 위한 그래프 산출

--- Script/consumption.R
+++ Script/consumption.R
@@ -2,7 +2,9 @@ library(ggplot2)
 library(showtext)
 library(gridExtra)
 library(showtext)
+library(ggrepel)
 library(sysfonts)
+library(tidyverse)
 font_add_google('Nanum Gothic', family='NanumGothic')
 showtext_auto()
 par(family="NanumGothic")
@@ -28,7 +30,7 @@ Type_palette <- c('건물지'='#DF0101',
                   '소성유구'='#FF00FF')
 
 all_pie <- data.frame(
-  Type = c("건물지", "구상유구", "기타", "분묘", "소성유구", "수혈", "제사유구", "주거지"),
+  Type = c("건물지", "구상유구", "기타",  "분묘", "소성유구", "수혈", "제사유구", "주거지"),
   value = c(length(which(df$유구성격 == "건물지")),
             length(which(df$유구성격 == "구상유구")),
             length(which(df$유구성격 == "기타")),
@@ -40,6 +42,13 @@ all_pie <- data.frame(
   percent = c(round(prop.table((table(df$유구성격)))*100))
 )
 
+all_pie_check <- all_pie %>% 
+  mutate(csum = rev(cumsum(rev(percent))), 
+         pos = percent/2 + lead(csum, 1),
+         pos = if_else(is.na(pos), percent/2, pos))
+
+a <- c(round(prop.table((table(df$유구성격)))*100))
+
 A_pie <- data.frame(
   Type = c("건물지", "구상유구", "기타", "분묘", "소성유구", "수혈", "제사유구", "주거지"),
   value = c(length(which(A_df$유구성격 == "건물지")),
@@ -50,7 +59,7 @@ A_pie <- data.frame(
             length(which(A_df$유구성격 == "수혈")),
             length(which(A_df$유구성격 == "제사유구")),
             length(which(A_df$유구성격 == "주거지"))),
-  percent = c("-", 15, 6, 22, "-", 18, "-", 40)
+  percent = c(0, 15, 6, 22, 0, 18, 0, 40)
 )
 
 B_pie <- data.frame(
@@ -63,7 +72,7 @@ B_pie <- data.frame(
             length(which(B_df$유구성격 == "수혈")),
             length(which(B_df$유구성격 == "제사유구")),
             length(which(B_df$유구성격 == "주거지"))),
-  percent = c(1, 11, 8, 0, "-", 56, 1, 22)
+  percent = c(1, 11, 8, 0, 0, 56, 1, 22)
 )
 
 C_pie <- data.frame(
@@ -76,7 +85,7 @@ C_pie <- data.frame(
             length(which(C_df$유구성격 == "수혈")),
             length(which(C_df$유구성격 == "제사유구")),
             length(which(C_df$유구성격 == "주거지"))),
-  percent = c("-", 8,  3, "-", "-", 26, "-", 64)
+  percent = c(0, 8,  3, 0, 0, 26, 0, 64)
 )
 
 D_pie <- data.frame(
@@ -89,7 +98,7 @@ D_pie <- data.frame(
             length(which(D_df$유구성격 == "수혈")),
             length(which(D_df$유구성격 == "제사유구")),
             length(which(D_df$유구성격 == "주거지"))),
-  percent = c(1,10, 6, 3, 1, 39, "-", 40)
+  percent = c(0, 10, 8, 3, 1, 39, 0, 40)
 )
 
 E_pie <- data.frame(
@@ -102,7 +111,7 @@ E_pie <- data.frame(
             length(which(E_df$유구성격 == "수혈")),
             length(which(E_df$유구성격 == "제사유구")),
             length(which(E_df$유구성격 == "주거지"))),
-  percent = c("-", "-", 6, 6, "-", 24, "-", 64)
+  percent = c(0, 0, 6, 6, 0, 24, 0, 64)
 )
 
 #####Visualization#####
@@ -110,12 +119,9 @@ E_pie <- data.frame(
 p_all <- ggplot(all_pie, aes(x='', y=value, fill=Type)) +
   geom_bar(stat='identity') +
   coord_polar('y', start=0) +
-  geom_text(aes(label = paste0(round(value), "(", percent, "%", ")")),
+  geom_text(aes(x=1.3, label = paste0(round(value), "(", percent, "%", ")")),
             position = position_stack(vjust=0.5),
-            colour = 'black', size = 5) +
-  #geom_label_repel(data = all_pie,
-  #                 aes(label = paste0(value, "%")),
-  #                 size = 4.5, nudge_x = 1, show.legend = FALSE)
+            colour = 'black', size = 5.5) +
   labs(x = NULL, y = NULL, title = "전체") +
   scale_fill_manual(values=Type_palette) +
   theme_classic() +
@@ -123,97 +129,106 @@ p_all <- ggplot(all_pie, aes(x='', y=value, fill=Type)) +
         axis.text = element_blank(),
         axis.ticks = element_blank(),
         plot.title = element_text(vjust = -5, hjust=0.5),
-        title = element_text(size=20))
+        title = element_text(size=20),
+        legend.text = element_text(size=11, color="black"))
 ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TESTall.png", dpi=150, width=5, height=5, units='in')
 
 #한강하류 막대그래프
-p1 <- ggplot(A_pie, aes(x=Type, y=value, fill=Type)) +
+p1 <- ggplot(A_pie, aes(x=Type, y=percent, fill=Type)) +
   geom_bar(stat='identity') +
-  scale_y_continuous(limits = c(0, 150), breaks = seq(0, 150, 10), labels = paste0(seq(0, 150, 10)))+
-  coord_flip() +
-  geom_text(aes(label = paste0(round(value), "(", percent, "%", ")")),
-            position = position_stack(vjust=1),
-            colour = 'black') +
-  labs(x = NULL, y = NULL) +
+  scale_y_continuous(limits = c(-2, 65), breaks = seq(-2, 65, 10), labels = paste0(seq(0, 65, 10)))+
+  geom_text(aes(label = paste0(round(percent), "%", "(", value, "개", ")")),
+            position = position_stack(vjust=0.5),
+            colour = 'black',
+            size = 6,
+            angle=90) +
+  labs(x = NULL, y = NULL, title="한강하류") +
   scale_fill_manual(values=Type_palette) +
   theme_bw() +
   theme(axis.line = element_blank(),
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 ),
         axis.ticks = element_blank(),
-        plot.title = element_text(vjust = 1, hjust=0.5, size=15),
+        plot.title = element_text(vjust = -10, hjust=0.1, size=25),
         legend.position = "none",
-        axis.text = element_text(size=12, colour="black"))
-ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TEST1.png", dpi=150, width=6, height=2, units='in')
+        axis.text = element_text(size=23, colour="black"))
+ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TEST1.png", dpi=150, width=3, height=6, units='in')
 
 #한강중류 막대그래프
-p2 <- ggplot(B_pie, aes(x=Type, y=value, fill=Type)) +
+p2 <- ggplot(B_pie, aes(x=Type, y=percent, fill=Type)) +
   geom_bar(stat='identity') +
-  scale_y_continuous(limits = c(0, 150), breaks = seq(0, 150, 10), labels = paste0(seq(0, 150, 10)))+
-  coord_flip() +
-  geom_text(aes(label = paste0(round(value), "(", percent, "%", ")")),
-            position = position_stack(vjust=1),
-            colour = 'black') +
-  labs(x = NULL, y = NULL) +
+  scale_y_continuous(limits = c(-2, 65), breaks = seq(-2, 65, 10), labels = paste0(seq(0, 65, 10)))+
+  geom_text(aes(label = paste0(round(percent), "%", "(", value, "개", ")")),
+            position = position_stack(vjust=0.5),
+            colour = 'black',
+            size = 6,
+            angle=90) +
+  labs(x = NULL, y = NULL, title="한강중류") +
   scale_fill_manual(values=Type_palette) +
   theme_bw() +
   theme(axis.line = element_blank(),
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 ),
         axis.ticks = element_blank(),
-        plot.title = element_text(vjust = 1, hjust=0.5, size=15),
+        plot.title = element_text(vjust = -10, hjust=0.1, size=25),
         legend.position = "none",
-        axis.text = element_text(size=12, colour="black"))
-ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TEST2.png", dpi=150, width=6, height=2, units='in')
+        axis.text = element_text(size=23, colour="black"))
+ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TEST2.png", dpi=150, width=3, height=6, units='in')
 
 #임진·한탄강+경기북부 막대그래프
-p3 <- ggplot(C_pie, aes(x=Type, y=value, fill=Type)) +
+p3 <- ggplot(C_pie, aes(x=Type, y=percent, fill=Type)) +
   geom_bar(stat='identity') +
-  scale_y_continuous(limits = c(0, 150), breaks = seq(0, 150, 10), labels = paste0(seq(0, 150, 10)))+
-  coord_flip() +
-  geom_text(aes(label = paste0(round(value), "(", percent, "%", ")")),
-            position = position_stack(vjust=1),
-            colour = 'black') +
-  labs(x = NULL, y = NULL) +
+  scale_y_continuous(limits = c(-2, 65), breaks = seq(-2, 65, 10), labels = paste0(seq(0, 65, 10)))+
+  geom_text(aes(label = paste0(round(percent), "%", "(", value, "개", ")")),
+            position = position_stack(vjust=0.5),
+            colour = 'black',
+            size = 6,
+            angle=90) +
+  labs(x = NULL, y = NULL, title="임진·한탄강+경기북부") +
   scale_fill_manual(values=Type_palette) +
   theme_bw() +
   theme(axis.line = element_blank(),
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 ),
         axis.ticks = element_blank(),
-        plot.title = element_text(vjust = 1, hjust=0.5, size=15),
+        plot.title = element_text(vjust = -10, hjust=0.1, size=25),
         legend.position = "none",
-        axis.text = element_text(size=12, colour="black"))
-ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TEST3.png", dpi=150, width=6, height=2, units='in')
+        axis.text = element_text(size=23, colour="black"))
+ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TEST3.png", dpi=150, width=3, height=6, units='in')
 
 #경기남부 막대그래프
-p4 <- ggplot(D_pie, aes(x=Type, y=value, fill=Type)) +
+p4 <- ggplot(D_pie, aes(x=Type, y=percent, fill=Type)) +
   geom_bar(stat='identity') +
-  scale_y_continuous(limits = c(0, 150), breaks = seq(0, 150, 10), labels = paste0(seq(0, 150, 10)))+
-  coord_flip() +
-  geom_text(aes(label = paste0(round(value), "(", percent, "%", ")")),
-            position = position_stack(vjust=1),
-            colour = 'black') +
-  labs(x = NULL, y = NULL) +
+  scale_y_continuous(limits = c(-2, 65), breaks = seq(-2, 65, 10), labels = paste0(seq(0, 65, 10)))+
+  geom_text(aes(label = paste0(round(percent), "%", "(", value, "개", ")")),
+            position = position_stack(vjust=0.5),
+            colour = 'black',
+            size = 6,
+            angle=90) +
+  labs(x = NULL, y = NULL, title="경기남부") +
   scale_fill_manual(values=Type_palette) +
   theme_bw() +
   theme(axis.line = element_blank(),
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 ),
         axis.ticks = element_blank(),
-        plot.title = element_text(vjust = 1, hjust=0.5, size=15),
+        plot.title = element_text(vjust = -10, hjust=0.1, size=25),
         legend.position = "none",
-        axis.text = element_text(size=12, colour="black"))
-ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TEST4.png", dpi=150, width=6, height=2, units='in')
+        axis.text = element_text(size=23, colour="black"))
+ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TEST4.png", dpi=150, width=3, height=6, units='in')
 
 #북한강·남한강 막대그래프
-p5 <- ggplot(E_pie, aes(x=Type, y=value, fill=Type)) +
+p5 <- ggplot(E_pie, aes(x=Type, y=percent, fill=Type)) +
   geom_bar(stat='identity') +
-  scale_y_continuous(limits = c(0, 150), breaks = seq(0, 150, 10), labels = paste0(seq(0, 150, 10)))+
-  coord_flip() +
-  geom_text(aes(label = paste0(round(value), "(", percent, "%", ")")),
-            position = position_stack(vjust=1),
-            colour = 'black') +
-  labs(x = NULL, y = NULL) +
+  scale_y_continuous(limits = c(-2, 65), breaks = seq(-2, 65, 10), labels = paste0(seq(0, 65, 10)))+
+  geom_text(aes(label = paste0(round(percent), "%", "(", value, "개", ")")),
+            position = position_stack(vjust=0.5),
+            colour = 'black',
+            size = 6,
+            angle=90) +
+  labs(x = NULL, y = NULL, title="북한강·남한강") +
   scale_fill_manual(values=Type_palette) +
   theme_bw() +
   theme(axis.line = element_blank(),
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5 ),
         axis.ticks = element_blank(),
-        plot.title = element_text(vjust = 1, hjust=0.5, size=15),
+        plot.title = element_text(vjust = -10, hjust=0.1, size=25),
         legend.position = "none",
-        axis.text = element_text(size=12, colour="black"))
-ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TEST5.png", dpi=150, width=6, height=2, units='in')
-
-grid.arrange(p1, p2, p3, p4, p5, ncol=1)
+        axis.text = element_text(size=23, colour="black"))
+ggsave("C:/github/P3K-HB_Pottery-Kiln/Graph/TEST5.png", dpi=150, width=3, height=6, units='in')
